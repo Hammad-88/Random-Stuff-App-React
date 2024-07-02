5ebd36877bbfd5e4bd9c0d3b02eb155f4e2677be
Hassan Ali <139656401+hasanbut17314@users.noreply.github.com>
2024-07-02 22:50:09 +0500

    added responsiveness to sidebar

--- src/comp/Header.jsx
+++ src/comp/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import logo from '../assets/logo_random.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
+
+  let toggleSidebar = () => {
+    document.querySelector('.sideBar').classList.toggle('zinda');
+    document.querySelector('.overlay').classList.toggle('hidden');
+  }
+
   return (
-    <div className='flex justify-center items-center'>
-        <img className='h-32' src={logo} alt="" />
+    <div className='flex lg:justify-center justify-between w-full md:px-8'>
+      <button onClick={toggleSidebar} className='flex lg:hidden text-2xl mt-4'>
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+      <img className='lg:h-32 md:h-28 lg:ms-auto' src={logo} alt="Logo" />
+      <div className='lg:ml-auto'>
+        
+      </div>
     </div>
   )
 }
